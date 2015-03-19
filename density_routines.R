49e2b709d8433712a08346dcf11e2033c9de121d
Gaik Tamazian <gaik.tamazian@gmail.com>
2015-03-19 15:40:36 +0300

    Function to build range windows added

---
+++ density_routines.R
@@ -0,0 +1,58 @@
+# Copyright (C) 2015 by Gaik Tamazian
+# gaik (dot) tamazian (at) gmail (dot) com
+
+# This script is a module containing routines to perform SNV density analysis.
+
+suppressPackageStartupMessages(library(GenomicRanges))
+
+get.windows <- function(regions, window.len,
+                        remainder.threshold=0.5*window.len, verbose=FALSE) {
+  # Given a GRanges object representing regions in a genome, return the GRanges
+  # object of genome windows of the specified length distributed in the
+  # regions.
+  #
+  # Arguments:
+  #   regions: a GRanges object of regions in a genome
+  #   window.len: a length of windows to be contructed for the specified genome
+  #     regions
+  #   remainder.threshold: the least length of a window situated in the end of
+  #     a region to be included in the result
+  #   verbose: show a progress bar indicating the window computation progress
+  #
+  # Returns:
+  #   The GRanges object of the constructed windows within the regions.
+  
+  result <- GRanges(seqinfo=seqinfo(regions))
+  
+  if (verbose) {
+    message(paste('Calculating windows of', window.len, 'bp in', 
+                  length(regions), 'regions...'))
+    pb <- txtProgressBar(min=0, max=length(regions), style=3)
+  }
+  
+  # We create vectors of sequence names, start and end positions for the given
+  # ranges to accelerate the loop below.
+  region.seq.names <- seqnames(regions)
+  region.start <- start(regions)
+  region.end <- end(regions)
+  for (i in 1:length(regions)) {
+    temp.start <- seq(region.start[i], region.end[i], by=window.len)
+    temp <- GRanges(seqnames=region.seq.names[i],
+                    ranges=IRanges(start=temp.start, width=window.len),
+                    seqinfo=seqinfo(regions))
+    # check the last window of the region
+    if (width(temp[length(temp)]) < remainder.threshold) {
+      temp <- temp[-length(temp)]
+    }
+    result <- c(result, temp)
+    if (verbose) {
+      setTxtProgressBar(pb, i)
+    }
+  }
+  
+  if (verbose) {
+    close(pb)
+  }
+  
+  return(result)
+}
